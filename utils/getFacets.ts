5174d47dbfac0f9f9a3c9b06666f46e8d96654f4
MejiasDev <josemejiasji@gmail.com>
2024-06-18 17:10:32 +0200

    chore: cleamup

--- utils/getFacets.ts
+++ utils/getFacets.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 // Load facets from the src/facets folder, return an array of facet names
-function getFacets(omit: string[] = ['DiamondCutFacet', 'DiamondLoupeFacet']) {
+function getFacets(omit: string[] = ['DiamondCutFacet']) {
   const facets = fs
     .readdirSync('./src/facets')
     .map((file) => path.parse(file).name)
